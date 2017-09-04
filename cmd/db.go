eade9cfe4180d8c1d0b40d4de98b1877cdb13d95
Steven <steven@rootedlogics.com>
2017-09-04 11:41:32 -0400

    Default

--- cmd/db.go
+++ cmd/db.go
@@ -23,13 +23,8 @@ import (
 // dbCmd represents the db command
 var dbCmd = &cobra.Command{
 	Use:   "db",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Listing of DB Information.",
+	Long: `Gathers Database information from your Drupal installation.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Work your own magic here
 		fmt.Println("db called")
