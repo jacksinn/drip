3f71649796900e0fee08ba77af4dd743b5525df3
Steven <steven@rootedlogics.com>
2017-09-24 23:44:34 -0400

    start fleshing out function calls

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,5 @@
 package config
 
 type Configuration struct {
-
+	//
 }
---
+++ internal/platform/command.go
@@ -0,0 +1,24 @@
+package platform
+
+func readSettingsFile() {
+	//
+}
+
+/*
+  Modules
+ */
+func moduleEnable(module string) {
+	//
+}
+
+func moduleDisable(module string) {
+	//
+}
+
+func moduleScaffold() {
+	//
+}
+
+func cacheClear(cache string) {
+	//
+}
