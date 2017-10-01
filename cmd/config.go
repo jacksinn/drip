53a4d6ec05608c39338ed8c39c40ee2d681d7242
Steven <steven@rootedlogics.com>
2017-10-01 17:16:02 -0400

    Default

--- cmd/config.go
+++ cmd/config.go
@@ -15,9 +15,10 @@
 package cmd
 
 import (
-	"fmt"
+	//"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/jacksinn/drip/internal/platform"
 )
 
 // configCmd represents the config command
@@ -30,10 +31,11 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
-		fmt.Println("config called")
-	},
+	Run: runConfig,
+}
+
+func runConfig(cmd *cobra.Command, args []string) {
+	platform.ReadSettingsFile()
 }
 
 func init() {
--- internal/platform/command.go
+++ internal/platform/command.go
@@ -1,10 +1,25 @@
 package platform
 
-func readSettingsFile() {
+import (
+	"log"
+	"os"
+	"fmt"
+	filepath "path/filepath"
+)
+
+func ReadSettingsFile() {
 	// Get Path to Settings file
 	// First, Assume in site root directory
+	
 	// Second, Try to get path to settings.php if in site dir (look for index.php moving backwards?)
 	// If we can't find the settings.php, return error
+
+	// Testing things below
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(dir)
 }
 
 /*
