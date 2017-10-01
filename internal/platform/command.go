38f1b15456ab3375959e5fd4a2d64b390610c47b
Steven <steven@rootedlogics.com>
2017-10-01 17:07:56 -0400

    notes for what to do

--- internal/platform/command.go
+++ internal/platform/command.go
@@ -1,7 +1,10 @@
 package platform
 
 func readSettingsFile() {
-	//
+	// Get Path to Settings file
+	// First, Assume in site root directory
+	// Second, Try to get path to settings.php if in site dir (look for index.php moving backwards?)
+	// If we can't find the settings.php, return error
 }
 
 /*
