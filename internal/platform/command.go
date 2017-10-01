d0c53121b11079ba2058b220838d46bb9379ed9f
Steven <steven@rootedlogics.com>
2017-10-01 17:36:49 -0400

    it now can check if site dir is there

--- internal/platform/command.go
+++ internal/platform/command.go
@@ -4,21 +4,45 @@ import (
 	"log"
 	"os"
 	"fmt"
+	"io/ioutil"
 )
 
 func ReadSettingsFile() {
-	// Get Path to Settings file
-	// First, Assume in site root directory
-	
-	// Second, Try to get path to settings.php if in site dir (look for index.php moving backwards?)
-	// If we can't find the settings.php, return error
-
-	// Testing things below
+	// Get Path to Settings
+	// Getting current working directory
 	dir, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(dir)
+
+	defaultSettingsFilePath := "sites/default/settings.php"
+	inDrupalDocroot := false
+
+	//Try to figure out site root
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		fmt.Println(f.Name())
+		// @todo Gotta be wary of "sites" dirs that are not from Drupal sites
+		if f.Name() == "sites" {
+			inDrupalDocroot = true
+		}
+	}
+
+	if inDrupalDocroot {
+		fmt.Println(defaultSettingsFilePath)
+	} else {
+		fmt.Println("Not in Drupal docroot. Have to do more.")
+	}
+	// First, Assume in site root directory
+	
+	// Second, Try to get path to settings.php if in site dir (look for index.php moving backwards?)
+	// If we can't find the settings.php, return error
+
 }
 
 /*
